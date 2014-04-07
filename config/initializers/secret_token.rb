125174e7067ee9d3bc3031f5e92a4361d74070dc
Andy von Dohren <avondohren@gmail.com>
2014-04-07 09:55:47 -0500

    inital build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+A2Checkin::Application.config.secret_token = 'bb42600300aa8cddb7c77590ad76d6db5d55df5edc0eed21ccdefd1b56399b343a1841d836d72edb58cd7fc7a6bf56921ca423832d688f4cd59c34cea814ca66'
